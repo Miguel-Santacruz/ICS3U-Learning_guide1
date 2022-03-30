4c462ae2e0cf277c3bf2797a65fd83e2127503ba
Miguel-Santacruz <99361445+Miguel-Santacruz@users.noreply.github.com>
2022-03-30 14:30:45 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created by: Miguel Santacruz
+# Created on: Mar 2022
+# This is the "Hello, world!" program on the PyBadge
+
+import ugame
+import stage
+
+
+def game_scene():
+    # this function is the main game_scene
+
+    print("\n\n\n") # 3 blank lines
+    print("Hello, World!")
+
+    # repeat forever, game loop
+    while True:
+        pass # just a place holder for now
+
+
+if __name__ == "__main__":
+    game_scene()
